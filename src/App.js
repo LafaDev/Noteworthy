099e6a549587a38ae135c957a6864ca9039807e3
axarvia <alexandrelopesconnection@gmail.com>
2023-03-24 19:24:38 -0300

    Fixed version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import React from "react";
 import RouterComponent from "./routes";
-import Login from './pages/login/login';
-import './pages/login/login.css'
+import Login from './pages/login/index';
+import './pages/login/index.css'
 
-import React from "react";
-import RouterComponent from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+
       <RouterComponent />
     </div>
   );
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -15,31 +15,13 @@ function Login(){
             <TextField id="outlined-basic" label="Password" variant="outlined" />
             <br />
             <br />
-	    //Placeholder function, replace with a mechanism to check if the login is valid?
-            <Button variant="contained" onClick={() => navigate('./editor')}>Log in</Button>
+
+            <Button variant="contained">Log in</Button>
 
             </div>
             </div>
         </div>
     
-        );
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
-function Login() {
-  const navigate = useNavigate();
-
-  return (
-    <div>
-      <h1>Login</h1>
-      <button
-        type="button"
-        onClick={ () => navigate('/editor') }
-      >
-        Go to editor
-      </button>
-    </div>
-  );
-}
+        )};
 
 export default Login;
