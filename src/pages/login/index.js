65b1bb1406820d73fdc717aea67a18a464cd2daa
axarvia <alexandrelopesconnection@gmail.com>
2023-03-24 23:33:57 -0300

    Added logo and signup

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,23 +2,32 @@ import TextField from '@mui/material/TextField'
 import React from "react";
 import { useNavigate} from "react-router-dom";
 import Button from '@mui/material/Button'
+import {Link} from 'react-router-dom';
+import logo from '../../images/logo.png'
 
 function Login(){
     return(
         <div className="login-page">
             <div className="login-box"> 
             <div className="login-fields">
-                            <h2>Noteworthy</h2>
-            <TextField id="outlined-basic" label="E-mail" variant="outlined" />
+            <div className= "login-logo">
+            <img src={logo} />
+            </div>
+            <div className='login-title'>
+                               <p>Noteworthy</p>
+                               </div>
+            <TextField id="outlined-basic" label="E-mail" variant="outlined" size="small" />
             <br />
             <br />
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
+            <TextField id="outlined-basic" label="Password" variant="outlined" size="small"/>
             <br />
             <br />
 
             <Button variant="contained">Log in</Button>
-
-            </div>
+	    <br />
+	    <br />
+	    <p>Not a user? <Link>Sign in</Link></p>
+	    </div>
             </div>
         </div>
     
