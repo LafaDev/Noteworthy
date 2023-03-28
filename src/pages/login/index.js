3d378ff914950ed252adcf9e4d627dc563fe9889
lafadev <lafa.dev@protonmail.com>
2023-03-28 19:28:44 -0300

    second slider

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,89 +1,181 @@
-import { useState } from 'react';
+import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import { useSpring, animated } from 'react-spring';
+// import { Link } from 'react-router-dom';
+import { useSpring, animated } from '@react-spring/web'
+import styles from './styles.module.css'
 
-function Login(){
+function Login() {
+  const [completed, setCompleted] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState(false);
   const [showEmailInput, setShowEmailInput] = useState(true);
   const [showPassInput, setShowPassInput] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-
-  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const handleEmailChange = (e) => setEmail(e.target.value);
-  const handlePassChange = (e) => setPassword(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const emailRegex = /^\S+@\S+\.\S+$/;
+
+  const left = {
+    bg: `linear-gradient(120deg, #f093fb 0%, #f5576c 100%)`,
+    justifySelf: 'end',
+  }
+  
+  const right = {
+    bg: `linear-gradient(120deg, #96fbc4 0%, #f9f586 100%)`,
+    justifySelf: 'start',
+  }
+
+  const [{ x, bg, scale }, api] = useSpring(() => ({
+    x: completed ? -300 : 0,
+    scale: 1,
+    ...left,
+  }))
+
+  const sliderProps = useSpring({
+    transform: completed ? 'translateX(50%)' : 'translateX(0%)',
+    config: {
+      tension: 200,
+      friction: 20,
+    },
+  });
 
   const emailProps = useSpring({
     opacity: showPassInput ? 0 : 1,
-    transform: showPassInput ? 'translate3d(-10%, 0, 0)' : 'translate3d(0, 0, 0)',
   });
 
   const passProps = useSpring({
     opacity: showPassInput ? 1 : 0,
-    transform: showPassInput ? 'translate3d(0, 0, 0)' : 'translate3d(10%, 0, 0)',
   });
 
-  const handleContinue = (e) => {
+  const handleButtonClick = () => {
+    if (!completed) {
+      api.start({
+        x: -300,
+        scale: 1,
+        ...right,
+      })
+      setCompleted(true);
+   }
+  }
+
+   const handleContinue = (e) => {
     e.preventDefault();
 
     if (emailRegex.test(email)) {
       setShowPassInput(true);
       setShowEmailInput(false);
       setEmailError(false);
+      handleButtonClick();
     } else {
       setEmailError(true);
     }
   }
 
   const handleBack = () => {
+    if (completed) {
+      api.start({
+          x: 0,
+          scale: 1,
+          ...right,
+      })
+      setCompleted(false);
+    }
+
     setShowPassInput(false);
     setShowEmailInput(true);
   }
 
-  return(
+  return (
     <div className="login-page">
-      <div className="login-box"> 
+      <div className="login-box">
         <div className='login-box-decoration' />
         <div className="horizontal_dotted_line" />
         <div className="login-fields">
-          <div className= "login-logo" />
-          <div className='login-title'>
-            <p>NoteWorthy</p>
-          </div>
-          <div className="email-field-wrapper">
-            {showEmailInput && (
-            <animated.div style={emailProps}>
-              <TextField
-                id="outlined-basic"
-                label="E-mail"
-                variant="outlined"
-                size="small"
-                error={emailError}
-                helperText={emailError && "Please enter a valid email"}
-                value={email}
-                onChange={handleEmailChange}
-              />
-            </animated.div>
-          )}
-          {showPassInput && (
-            <animated.div style={passProps}>
-              <TextField
-                id="outlined-basic"
-                label="Password"
-                type="password"
-                variant="outlined"
-                size="small"
-                value={password}
-                onChange={handlePassChange}
-              />
-            </animated.div>
-          )}
-          </div>
-          <div className='buttons-container'>
-            {showPassInput && (
+          <div className="login-logo" />
+            <div className='login-title'>
+              <p>NoteWorthy</p>
+            </div>
+            <div className="sliderUpperContainer" style={{ position: 'relative' }}>
+              <animated.div className={styles.sliderContainer} style={sliderProps}>
+                <div className={styles.inputContainer} />
+                <animated.div className={styles.item} style={{ background: bg }}>
+                  <animated.div className={styles.fg} style={{ x, scale }}>
+                    {showEmailInput ? (
+                      <animated.div style={emailProps}>
+                        <TextField
+                          id="filled-basic"
+                          label="E-mail"
+                          variant="standard"
+                          InputLabelProps={{
+                              sx: {
+                                fontSize: '16px',
+                                fontFamily: 'Roboto, sans-serif',
+                                color: 'white',
+                                '&.Mui-focused': {
+                                  color: 'white',
+                                },
+                              },
+                            }}
+                          InputProps={{
+                            sx: {
+                              fontSize: '16px',
+                              fontFamily: 'Roboto, sans-serif',
+                              color: 'white',
+                              '&:before': {
+                                borderBottomColor: 'white',
+                              },
+                              '&:after': {
+                                borderBottomColor: 'yellow',
+                              },
+                              '&:hover:not(.Mui-disabled):before': {
+                                borderBottomColor: 'white',
+                              },
+                            },
+                          }}
+                          size="normal"
+                          error={emailError}
+                          helperText={emailError && "Please enter a valid email"}
+                          value={email}
+                          onChange={handleEmailChange}
+                        />
+                      </animated.div>
+                    ) : ( <p className="emailInCard">{email}</p>)}
+                  </animated.div>
+                  {showPassInput && (
+                    <animated.div style={passProps}>
+                      <TextField
+                        id="outlined-basic"
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        size="small"
+                        value={password}
+                        onChange={handlePasswordChange}
+                      />
+                    </animated.div>
+                  )}
+                </animated.div>
+              </animated.div>
+            </div>
+            <div className='buttons-container'>
+              {showPassInput && (
+                <Button
+                  variant="contained"
+                  style={{
+                    backgroundColor: "#e4af09",
+                    fontSize: "12px",
+                    margin: "4px",
+                    alignItems: "center",
+                    borderRadius: "16px"
+                  }}
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+              )}
               <Button
                 variant="contained"
                 style={{
@@ -93,32 +185,15 @@ function Login(){
                   alignItems: "center",
                   borderRadius: "16px"
                 }}
-                onClick={handleBack}
+                onClick={handleContinue}
               >
-                Back
+                Continue
               </Button>
-            )}
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "#e4af09",
-                fontSize: "12px",
-                margin: "4px",
-                alignItems: "center",
-                borderRadius: "16px"
-              }}
-              onClick={handleContinue}
-            >
-              Continue
-            </Button>
           </div>
-          <p>
-            Not a user? <Link>Sign Up</Link>
-          </p>
-	      </div>
+        </div>
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default Login;
