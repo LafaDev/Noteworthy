54a2d238656e41654ebdd0ac0077240d20b2dd6d
lafadev <lafa.dev@protonmail.com>
2023-03-28 00:21:54 -0300

    fixing major issues

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,57 +1,64 @@
 import TextField from '@mui/material/TextField'
-import React from "react";
-import { useNavigate} from "react-router-dom";
 import Button from '@mui/material/Button'
 import {Link} from 'react-router-dom';
-import logo from '../../images/logo.png'
-import { borderRadius } from '@mui/system';
 
 
 const style = {  
-    "& label.Mui-focused": {
+  "& label.Mui-focused": {
     color: "#f5c934"
   },
-    "& .MuiOutlinedInput-root": {
-      "&.Mui-focused fieldset": {
-        borderColor: "#f5c934"
-      }
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "#f5c934"
     }
-  }   
-  
-  
+  }
+}   
 
 function Login(){
-    return(
-        <div className="login-page">
-            <div className="login-box"> 
-            <div className='login-box-decoration'></div>
-            <div class="horizontal_dotted_line"></div>
-            <div className="login-fields">
-            <div className= "login-logo">            </div>
-            <div className='login-title'>
-                               <p>NoteWorthy</p>
-                               </div>
-            <TextField id="outlined-basic" label="E-mail" variant="outlined"  size="small" sx={style}/>
-            <br />
-            <br />
-            <TextField id="outlined-basic" label="Password" variant="outlined" size="small" sx={style}/>
-            <br />
-            <br />
-
-            <Button variant="contained" style={{
+  return(
+    <div className="login-page">
+      <div className="login-box"> 
+        <div className='login-box-decoration' />
+        <div className="horizontal_dotted_line" />
+        <div className="login-fields">
+          <div className= "login-logo" />
+          <div className='login-title'>
+            <p>NoteWorthy</p>
+          </div>
+          <TextField
+            required
+            id="outlined-basic"
+            label="E-mail"
+            variant="outlined"
+            size="small"
+            sx={style}
+          />
+          <br /> <br />
+          <TextField
+            required
+            id="outlined-basic"
+            label="Password"
+            type="password"
+            variant="outlined"
+            size="small"
+            sx={style}
+          />
+          <br /> <br />
+          <Button variant="contained" style={{
             backgroundColor: "#e4af09",
             fontSize: "12px",
             margin: "4px",
             alignItems: "center",
             borderRadius: "16px"
-        }}>Log in</Button>
-	    <br />
-	    <br />
-	    <p>Not a user? <Link>Sign in</Link></p>
-	    </div>
-            </div>
-        </div>
-    
-        )};
+           }}>
+            Log in
+          </Button>
+	        <br /> <br />
+	        <p>Not a user? <Link>Sign in</Link></p>
+	      </div>
+      </div>
+    </div>
+  )
+};
 
 export default Login;
