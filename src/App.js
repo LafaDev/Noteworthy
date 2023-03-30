326aa4cc2e025b57c62cd08da362a63cbfe41b7e
lafadev <lafa.dev@protonmail.com>
2023-03-29 22:59:23 -0300

    margin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from "react";
 import RouterComponent from "./routes";
-import Login from './pages/login/index';
 import './pages/login/index.css'
 
 
 function App() {
   return (
     <div className="App">
-
       <RouterComponent />
     </div>
   );
